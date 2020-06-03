95591263f0a4dd28d186cfd51f4f1126e9e31769
rishabh-live <rishabh12536@gmail.com>
2020-06-03 09:14:03 +0530

    Nav Bar Updated

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ function spr() {
 	// body...
 	document.getElementById('container').innerHTML = "<br><h1>Running Test 3<h1><br>"
 }
+
+function navBarToggle() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
